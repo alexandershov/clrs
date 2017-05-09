99ec97ff18a36b660ec20ea83b006090ffe55f64
aershov <codumentary.com@gmail.com>
2017-05-09 11:53:32 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='clrs',
+    version='0.1.0',
+    packages=find_packages(),
+    tests_require=[
+        'pytest==3.0.7',
+    ],
+)
